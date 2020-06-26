81ebf4b3e7b9ca35fa6ca9bc4ade7189196fb504
Максим Шаптала <max_shaptala@MacBook-Pro-Maksim.local>
2020-06-26 18:03:20 +0300

    Project setup

---
+++ TwitterSlideOutMenu/TwitterSlideOutMenu/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TwitterSlideOutMenu
+//
+//  Created by Максим Шаптала on 25.06.2020.
+//  Copyright © 2020 Максим Шаптала. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
+        title = "Home"
+    }
+
+
+    
+}
+
