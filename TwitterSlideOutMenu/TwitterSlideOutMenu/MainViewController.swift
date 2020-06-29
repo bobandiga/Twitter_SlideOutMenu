d91abc9d70b1a090a5de3a5dc97271407e87bb02
Максим Шаптала <max_shaptala@MacBook-Pro-Maksim.local>
2020-06-29 14:17:26 +0300

    Dragging animation

--- TwitterSlideOutMenu/TwitterSlideOutMenu/MainViewController.swift
+++ TwitterSlideOutMenu/TwitterSlideOutMenu/MainViewController.swift
@@ -15,13 +15,13 @@ class MainViewController: UITableViewController {
         view.backgroundColor = #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
         
         setupNavigationItem()
+        setupMenuViewController()
         
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(panHandle(_:)))
+        view.addGestureRecognizer(panGesture)
         
-        guard let window = UIApplication.shared.windows.first else { return }
-        window.addSubview(menuViewController.view)
-        menuViewController.view.frame = CGRect(x: -menuWidth, y: 0, width: menuWidth, height: window.frame.height)
-        addChild(menuViewController)
-        
+        panGesture.minimumNumberOfTouches = 1
+        panGesture.maximumNumberOfTouches = 1
     }
     
     fileprivate var menuWidth: CGFloat = 300
@@ -38,48 +38,89 @@ class MainViewController: UITableViewController {
         return cell
     }
 
+    
+    var menuIsOpen = false
+    
 }
 
 fileprivate extension MainViewController {
     
     func setupNavigationItem() {
         title = "Home"
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openHandle(_:)))
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Hide", style: .plain, target: self, action: #selector(hideHandle(_:)))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openHandle))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Hide", style: .plain, target: self, action: #selector(hideHandle))
     }
     
-    @objc
-    func openHandle(_ sender: UIBarButtonItem) {
-        let translationX = menuWidth
+    func setupMenuViewController() {
+        guard let window = UIApplication.shared.windows.first else { return }
+        window.addSubview(menuViewController.view)
+        menuViewController.view.frame = CGRect(x: -menuWidth, y: 0, width: menuWidth, height: window.frame.height)
+        addChild(menuViewController)
+    }
+    
+    func performAnimation(transform: CGAffineTransform) {
         UIView.animate(withDuration: 0.5,
                        delay: 0,
                        usingSpringWithDamping: 1,
                        initialSpringVelocity: 1,
                        options: [.curveEaseOut],
                        animations: { [weak self] in
-                        self?.menuViewController.view.transform = CGAffineTransform.init(translationX: translationX, y: 0)
-                        self?.navigationController?.view.transform = CGAffineTransform.init(translationX: translationX, y: 0)
-                        //self?.view.transform = CGAffineTransform.init(translationX: translationX, y: 0)
+                        self?.menuViewController.view.transform = transform
+                        self?.navigationController?.view.transform = transform
         }) { (finished) in
-            print("Open animation: ", finished)
+            
         }
     }
     
     @objc
-    func hideHandle(_ sender: UIBarButtonItem) {
-        UIView.animate(withDuration: 0.3,
-                       delay: 0,
-                       usingSpringWithDamping: 1,
-                       initialSpringVelocity: 1,
-                       options: [.curveEaseIn],
-                       animations: { [weak self] in
-                        self?.menuViewController.view.transform = .identity
-                        self?.navigationController?.view.transform = .identity
-                        //self?.view.transform = .identity
-        }) { (finished) in
-            print("Dismiss animation: ", finished)
+    func openHandle() {
+        menuIsOpen = true
+        performAnimation(transform: CGAffineTransform.init(translationX: menuWidth, y: 0))
+    }
+    
+    @objc
+    func hideHandle() {
+        menuIsOpen = false
+        performAnimation(transform: .identity)
+    }
+    
+    @objc
+    func panHandle(_ sender: UIPanGestureRecognizer) {
+        
+        var tx = sender.translation(in: view).x
+        
+        if menuIsOpen {
+            tx += menuWidth
+        }
+        
+        //min(menuWidth, tx) запретит выдвигать меню вправо
+        //max - запретит выезжать влево
+        
+        tx = max(0, min(menuWidth, tx))
+        
+        performAnimation(transform: CGAffineTransform(translationX: tx, y: 0))
+        
+        if case .ended = sender.state {
+            handleEnded(sender)
         }
     }
     
+    func handleEnded(_ gesture: UIPanGestureRecognizer) {
+        let tx = gesture.translation(in: view).x
+        if menuIsOpen {
+            if abs(tx) < menuWidth / 4 {
+                openHandle()
+            } else {
+                hideHandle()
+            }
+        } else {
+            if tx < menuWidth / 2 {
+                hideHandle()
+            } else {
+                openHandle()
+            }
+        }
+        
+    }
 }
 
